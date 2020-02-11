6cbdb09eed1a1872f6df5d0574a000663d167cb2
Traci Crawford <tracicrawford@Tracis-MacBook-Air.local>
2020-02-10 18:13:00 -0600

    Redux 1 Completed

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import './Author.css';
+import store, {UPDATE_AUTHOR_FIRST, UPDATE_AUTHOR_LAST} from './../../store'
+
 
 class Author extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState()
     this.state = {
-      authorFirst: '',
-      authorLast: ''
+      authorFirst: reduxState.authorFirst,
+      authorLast: reduxState.authorLast
     };
   }
 
@@ -24,6 +27,14 @@ class Author extends Component {
   }
   saveChanges() {
     // Send data to Redux state
+    store.dispatch({
+      type: UPDATE_AUTHOR_FIRST,
+      payload: this.state.authorFirst
+    })
+    store.dispatch ({
+      type: UPDATE_AUTHOR_LAST,
+      payload: this.state.authorLast
+    })
   }
   render() {
     return (
